0aecfa748d9bf7fcab47d800f870e4b41a413bf2
soleilshane <auxilio.sherry@gmail.com>
2024-07-14 15:54:20 +0800

    feat(ui):add landing page and pages

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+var togglebtn = document.querySelector(".togglebtn");
+var nav = document.querySelector(".nav-links");
+var links = document.querySelector(".nav-links li");
+
+togglebtn.addEventListener("click", function() {
+    this.classList.toggle("click");
+    nav.classList.toggle("open");
+});
+
